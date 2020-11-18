7f7a5b3bdadbb089112272946e55edd6a6521c9e
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-18 13:16:56 -0800

    final

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,8 @@
 # your code goes here
+require 'pry'
 class Person
 
-    attr_reader :name #:happiness, :hygiene
+    attr_reader :name
     attr_accessor :bank_account, :happiness
 
     def initialize(name)
@@ -13,33 +14,26 @@ class Person
 
 
     def happiness
-       @happiness
+       @happiness.clamp(0,10)
     end
 
+    def happiness=(value)
+        @happiness = value
+    end
+   
     def happy?
         @happiness > 7 ? true : false
     end
 
-    # def happiness=(increment)
-    #   if @happiness + increment > 10
-    #     @happiness = 10
-    #   elsif @happiness + increment < 0
-    #     @happiness = 0
-    #   else
-    #     @happiness += increment
-    #     binding.pry
-    #   end
-
-    # end
     
 
 
     def hygiene
-        @hygiene
+        @hygiene.clamp(0,10)
     end
 
-    def hygiene=(increment)
-        @hygiene += increment
+    def hygiene=(value)
+        @hygiene = value
     end
 
     def clean?
@@ -48,45 +42,58 @@ class Person
 
     
 
-    # def get_paid(salary)
-    #     @bank_account += salary
-    #     "all about the benjamins"
-    # end
+     def get_paid(salary)
+         @bank_account += salary
+         "all about the benjamins"
+     end
 
-    # def take_bath
-    #     hygiene=(4)
-    #     "♪ Rub-a-dub just relaxing in the tub ♫"
-    # end
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
 
-    # def work_out
-    #     happiness=(3)
-    #     @hygiene -= 3
-    #     "♪ another one bites the dust ♫"
-    # end
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
 
 
-    # def call_friend(friend)
-    #     friend.happiness=(3)
-    #     happiness=(3)
-    #     "Hi #{friend.name}! It's #{@name}. How are you?"
-    # end
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness +=3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
 
-    # def start_conversation(friend, topic)
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            friend.happiness -= 2
+            self.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness += 1
+            self.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
 
-    # case topic
-    #     when (topic == "politics")
-    #         friend.happiness -= 1
-    #          @happiness -= 1
-    #         "blah blah partisan blah lobbyist"
-    #     when (topic == "weather")
-    #         friend.happiness += 1 
-    #          @happiness += 1 
-    #         "blah blah sun blah rain"
-    #     else
-    #         "blah blah blah blah blah"
-    #     end
-    # #topic == "politics" ? (friend.happiness -= 1 ; @happiness -= 1 ; return "blah blah partisan blah lobbyist") : nil
-    # #topic == "weather" ? (friend.happiness += 1 ; @happiness += 1 ; return "blah blah sun blah rain")
-    # end
+       
+        # case topic
+
+        #     when topic == 'politics'
+        #         friend.happiness -= 1
+        #         self.happiness -= 1
+        #         return "blah blah partisan blah lobbyist"
+        # when (topic == "weather")
+        #     friend.happiness += 1 
+        #      self.happiness += 1 
+        #     "blah blah sun blah rain"
+        # else
+        #     "blah blah blah blah blah"
+        # end
+    #topic == "politics" ? (friend.happiness -= 1 ; @happiness -= 1 ; return "blah blah partisan blah lobbyist") : nil
+    #topic == "weather" ? (friend.happiness += 1 ; @happiness += 1 ; return "blah blah sun blah rain")
+    end
 
 end
